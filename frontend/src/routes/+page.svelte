e3c5eb18a77191e5e9047fa3b47d9c4599897d50
aDiThYa-808 <adithyas824@gmail.com>
2025-10-27 20:58:19 +0530

    feat: call the health endpoint and display response

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
     const pageName = "home"
+    let responseText = ""
+
+    async function serverHealthCheck(){
+        const response = await fetch("http://localhost:3000/health")
+        const text = await response.text()
+        responseText = text
+    }
 </script>
 
 <h1>You are in {pageName} page</h1>
+<button onclick={serverHealthCheck}>Server Health Check</button>
+<h1>{responseText}</h1>
