df51059fb542d781ba1db24c5f2f5505abf4ce48
aDiThYa-808 <adithyas824@gmail.com>
2025-10-29 21:40:47 +0530

    feat: add helper to check if project is running locally or no

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,18 +3,37 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 
 	handlers "github.com/aDiThYa-808/persona-box/internal/handlers"
 	middlewares "github.com/aDiThYa-808/persona-box/internal/middlewares"
 )
+
+// helpers
+func isLambda() bool {
+	// Check for an aws env which will not be present locally.
+	_, exists := os.LookupEnv("AWS_LAMBDA_FUNCTION_NAME")
+	return exists
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.Handle("/health", http.HandlerFunc(handlers.HealthHandler))
 
+	// add cors headers
 	handler := middlewares.WithCors(mux)
 
-	log.Println("Server Running in port 3000...")
-	log.Fatal(http.ListenAndServe(":3000", handler))
+	// local environment
+	if !isLambda() {
+		log.Println("Server Running locally in port 3000...")
+		log.Fatal(http.ListenAndServe(":3000", handler))
+	}
+
+	adapter := httpadapter.New(handler)
+	lambda.Start(adapter.ProxyWithContext)
+
 }
