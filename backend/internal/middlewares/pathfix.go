be7a9f06e0656bdd61936a99f18e886995a9db5f
aDiThYa-808 <adithyas824@gmail.com>
2025-10-30 21:22:43 +0530

    feat: log the URL path prefix only if its found

--- backend/internal/middlewares/pathfix.go
+++ backend/internal/middlewares/pathfix.go
@@ -10,8 +10,8 @@ func StripStagePrefix(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Incomming path: %v", r.URL.Path)
 		trimmedPath, exists := strings.CutPrefix(r.URL.Path, "/default")
-		log.Printf("Trimmed prefix: %v", trimmedPath)
 		if exists {
+			log.Printf("Trimmed prefix: %v", trimmedPath)
 			r.URL.Path = trimmedPath
 		}
 
