a082c7ed80b56744e6bd0da5713753a38e237cb7
aDiThYa-808 <adithyas824@gmail.com>
2025-11-02 15:46:56 +0530

    feat: Read req body with error handling

--- backend/internal/handlers/chat.go
+++ backend/internal/handlers/chat.go
@@ -2,7 +2,11 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+
+	"github.com/aDiThYa-808/persona-box/internal/httpx"
 )
 
 type ChatRequest struct {
@@ -13,12 +17,18 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 	var req ChatRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
 
-	if err != nil {
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(map[string]string{
-			"error": "Missing 'message' field in the request body",
-		})
+	if err != nil || req.Message == "" {
+		httpx.WriteJSONError(w, "Missing 'message' field in the request body", http.StatusBadRequest)
+		return
 	}
 
 	defer r.Body.Close()
+
+	openaiKey := os.Getenv("OPENAI_SECRET_KEY")
+
+	if openaiKey == "" {
+		httpx.WriteJSONError(w, "Internal Server Error", http.StatusInternalServerError)
+		log.Println("Couldnt find openai secret key")
+		return
+	}
 }
