49d88792f7d520774d75382c85dffd2fa6e6391d
aDiThYa-808 <adithyas824@gmail.com>
2025-11-09 18:49:38 +0530

    feat: parse jwt token with claims and verify it

--- backend/internal/handlers/authgoogle.go
+++ backend/internal/handlers/authgoogle.go
@@ -2,59 +2,94 @@ package handlers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/MicahParks/keyfunc"
+	"github.com/golang-jwt/jwt/v4"
 
 	"github.com/aDiThYa-808/persona-box/internal/httpx"
 )
 
-type AuthRequest struct {
+type handler struct {
+	JWKS *keyfunc.JWKS
+}
+
+// constructor for dependency injection
+func New(jwks *keyfunc.JWKS) *handler {
+	return &handler{JWKS: jwks}
+}
+
+type authRequest struct {
 	IdToken string `json:"id_token"`
 }
 
-type AuthResponse struct {
-	AUD            string `json:"aud"`
-	ISS            string `json:"iss"`
-	SUB            string `json:"sub"`
-	Email          string `json:"email"`
-	Email_Verified string `json:"email_verified"`
-	Name           string `json:"name"`
-	IAT            string `json:"iat"`
-	EXP            string `json:"exp"`
-	JTI            string `json:"jti"`
-}
+func (h *handler) GoogleAuthHandler(w http.ResponseWriter, r *http.Request) {
 
-func GoogleAuthHandler(w http.ResponseWriter, r *http.Request) {
-	var AuthReq AuthRequest
-	decodeReqErr := json.NewDecoder(r.Body).Decode(&AuthReq)
+	var authReq authRequest
+	decodeReqErr := json.NewDecoder(r.Body).Decode(&authReq)
 
-	if decodeReqErr != nil || AuthReq.IdToken == "" {
-		httpx.WriteJSONError(w, decodeReqErr.Error(), http.StatusBadRequest)
+	if decodeReqErr != nil || authReq.IdToken == "" {
+		httpx.WriteJSONError(w, "Invalid request body", http.StatusBadRequest)
+		log.Println("Could not decode request body.")
 		return
 	}
 
 	defer r.Body.Close()
 
-	verifyUrl := "https://oauth2.googleapis.com/tokeninfo?id_token=" + AuthReq.IdToken
+	claims := &jwt.RegisteredClaims{}
 
-	resp, verifyErr := http.Get(verifyUrl)
-	if verifyErr != nil {
-		httpx.WriteJSONError(w, verifyErr.Error(), http.StatusInternalServerError)
+	token, parseErr := jwt.ParseWithClaims(authReq.IdToken, claims, h.JWKS.Keyfunc)
+
+	if parseErr != nil {
+		httpx.WriteJSONError(w, "Unauthorized", http.StatusUnauthorized)
+		log.Println("ParseWithClaims failed.")
 		return
 	}
 
-	if resp.StatusCode != 200 {
-		httpx.WriteJSONError(w, "Token is invalid, expired or malformed", http.StatusUnauthorized)
-	}
-
-	var AuthRes AuthResponse
-	decodeResErr := json.NewDecoder(resp.Body).Decode(&AuthRes)
-	if decodeResErr != nil {
-		httpx.WriteJSONError(w, "Couldnt verify token", http.StatusInternalServerError)
+	if !token.Valid {
+		httpx.WriteJSONError(w, "Unauthorized", http.StatusUnauthorized)
+		log.Println("Token not valid")
 		return
 	}
 
-	resp.Body.Close()
+	if !verifyClaims(claims) {
+		httpx.WriteJSONError(w, "Unauthorized", http.StatusUnauthorized)
+		log.Println("Invalid Audience, Issuer or ExpiresAt")
+		return
+	}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(AuthRes)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Valid JWT Token\n"))
+}
+
+// helpers
+func isAudienceValid(Audience jwt.ClaimStrings) bool {
+	audValid := false
+	clientId := os.Getenv("GOOGLE_CLIENT_ID")
+	for _, a := range Audience {
+		if a == clientId {
+			audValid = true
+			break
+		}
+	}
+	return audValid
+}
+
+func verifyClaims(claims *jwt.RegisteredClaims) bool {
+	if !isAudienceValid(claims.Audience) {
+		return false
+	}
+
+	if claims.Issuer != "https://accounts.google.com" && claims.Issuer != "accounts.google.com" {
+		return false
+	}
+
+	if claims.ExpiresAt == nil || claims.ExpiresAt.Time.Before(time.Now()) {
+		return false
+	}
+
+	return true
 }
