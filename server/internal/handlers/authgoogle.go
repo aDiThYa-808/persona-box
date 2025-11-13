5afc94d2559a8f2684cdd2b5ec3bde677ea0193d
aDiThYa-808 <adithyas824@gmail.com>
2025-11-13 20:53:54 +0530

    feat: create signed token for client and return it

--- server/internal/handlers/authgoogle.go
+++ server/internal/handlers/authgoogle.go
@@ -23,10 +23,21 @@ type authRequest struct {
 	IdToken string `json:"id_token"`
 }
 
+type authResponse struct {
+	Token string `json:"token"`
+	Email string `json:"email"`
+}
+
 type googleClaims struct {
 	Sub           string `json:"sub"`
 	Email         string `json:"email"`
 	EmailVerified bool   `json:"email_verified"`
+	jwt.RegisteredClaims
+}
+
+type tokenClaims struct {
+	Sub   string `json:"sub"`
+	Email string `json:"email"`
 	jwt.RegisteredClaims
 }
 
@@ -50,11 +61,11 @@ func (h *handler) GoogleAuthHandler(w http.ResponseWriter, r *http.Request) {
 
 	claims := &googleClaims{}
 
-	token, parseErr := jwt.ParseWithClaims(authReq.IdToken, claims, h.JWKS.Keyfunc)
+	token, parseErr := jwt.ParseWithClaims(authReq.IdToken, claims, h.JWKS.Keyfunc, jwt.WithValidMethods([]string{"RS256"}))
 
-	if parseErr != nil {
+	if parseErr != nil || token == nil {
 		httpx.WriteJSONError(w, "Unauthorized", http.StatusUnauthorized)
-		log.Println("ParseWithClaims failed.")
+		log.Println("ParseWithClaims returns error or token = nil.")
 		return
 	}
 
@@ -76,8 +87,32 @@ func (h *handler) GoogleAuthHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Valid JWT Token\n"))
+	tc := tokenClaims{
+		Sub:   claims.Sub,
+		Email: claims.Email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "personabox",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(7 * 24 * time.Hour)),
+		},
+	}
+
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, tc)
+	signingKey := os.Getenv("PERSONABOX_SECRET")
+	signedToken, tokenErr := newToken.SignedString([]byte(signingKey))
+
+	if tokenErr != nil {
+		httpx.WriteJSONError(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	authRes := authResponse{
+		Token: signedToken,
+		Email: claims.Email,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(authRes)
 }
 
 // helpers
