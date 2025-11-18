3877ecc798ba41f2abbf6b05cb3cd4db03081791
aDiThYa-808 <adithyas824@gmail.com>
2025-11-18 20:43:12 +0530

    feat: add keyfunc method to return signing key for parseWithClaims()

---
+++ server/internal/auth/keyfunc.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"errors"
+	"os"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func keyFunc(t *jwt.Token) (interface{}, error) {
+	_, ok := t.Claims.(*JWTClaims)
+	if !ok {
+		return nil, errors.New("unexpected claims type")
+	}
+
+	if _, methodOk := t.Method.(*jwt.SigningMethodHMAC); !methodOk {
+		return nil, errors.New("invalid signing method")
+	}
+
+	signingKey := os.Getenv("PERSONABOX_SECRET")
+
+	if signingKey == "" {
+		return nil, errors.New("signing key not found")
+	}
+
+	return []byte(signingKey), nil
+}
