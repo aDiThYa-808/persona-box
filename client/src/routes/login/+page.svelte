e5b2d48c365f7ed3b51cb9f2960d7d52ff07ed19
aDiThYa-808 <adithyas824@gmail.com>
2025-11-20 18:43:24 +0530

    feat: make POST request to backend for JWT verification

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { PUBLIC_GOOGLE_CLIENT_ID } from "$env/static/public";
+    import {PUBLIC_AWS_INVOKE_URL} from "$env/static/public"
     import { onMount } from "svelte";
 
     const clientId = PUBLIC_GOOGLE_CLIENT_ID
+    const invokeURL = PUBLIC_AWS_INVOKE_URL
 
     onMount(()=>{
         window.google.accounts.id.initialize({
             client_id: clientId,
-            callback: handleCredentialResponse
+            callback: handleResponse
         })
 
         window.google.accounts.id.renderButton(
@@ -16,9 +18,37 @@
         )
     })
 
-    function handleCredentialResponse(response:{credential:string}){
+    function handleResponse(response:{credential:string}){
         const idToken = response.credential
-        console.log("Google id token: ",idToken)
+        verifyJWT(idToken)
+    }
+
+    async function verifyJWT(jwt: string){
+        try{
+            const response = await fetch(`${invokeURL}/auth/google`,{
+                method:'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify({
+                    'id_token': jwt
+                })
+            })
+
+            const data = await response.json()
+
+            if(!response.ok){
+                throw new Error(`Authentication failed : ${data.error}`)
+            }
+
+            console.log("JWT verified successfully. Authentication complete")
+            console.log(data.sub)
+            console.log(data.access_token)
+            console.log(data.email)
+
+        }catch(err:unknown){
+            console.log((err as Error).message)
+        }
     }
 
 </script>
