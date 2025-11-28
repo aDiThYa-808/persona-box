50f3602bfa25f2a896483a0dbfc6dc00b2b1182a
aDiThYa-808 <adithyas824@gmail.com>
2025-11-28 20:00:38 +0530

    feat: recieve user object and display email

--- client/src/routes/dashboard/+page.svelte
+++ client/src/routes/dashboard/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+	export let data
+
     const pageName = "dashboard"
+    const email = data.user.email
 </script>
 
 <h1>You are in {pageName} page</h1>
+<p>Logged in using : {email}</p>
