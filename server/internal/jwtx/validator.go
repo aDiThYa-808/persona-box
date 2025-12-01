e3595766199d7d400f6a5c91bc03def96f603775
aDiThYa-808 <adithyas824@gmail.com>
2025-12-01 18:46:53 +0530

    feat: change method signature

--- server/internal/jwtx/validator.go
+++ server/internal/jwtx/validator.go
@@ -2,7 +2,6 @@ package jwtx
 
 import (
 	"errors"
-	"time"
 
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -13,10 +12,10 @@ type JWTClaims struct {
 	jwt.RegisteredClaims
 }
 
-func ValidateRequest(idToken string) (JWTClaims, error) {
+func ValidateAccessToken(accessToken string) (JWTClaims, error) {
 
 	claims := &JWTClaims{}
-	token, parseErr := jwt.ParseWithClaims(idToken, claims, keyFunc, jwt.WithValidMethods([]string{"HS256"}))
+	token, parseErr := jwt.ParseWithClaims(accessToken, claims, keyFunc, jwt.WithValidMethods([]string{"HS256"}))
 
 	if parseErr != nil || token == nil {
 		return JWTClaims{}, errors.New("unauthorized")
@@ -29,32 +28,9 @@ func ValidateRequest(idToken string) (JWTClaims, error) {
 	verifyErr := verifyClaims(*claims)
 
 	if verifyErr != nil {
+
 		return JWTClaims{}, verifyErr
 	}
 
 	return *claims, nil
-}
-
-func verifyClaims(claims JWTClaims) error {
-	if claims.Sub == "" {
-		return errors.New("unauthorized")
-	}
-
-	if claims.Email == "" {
-		return errors.New("unauthorized")
-	}
-
-	if claims.Issuer != "personabox" {
-		return errors.New("unauthorized")
-	}
-
-	if claims.ExpiresAt == nil || time.Now().After(claims.ExpiresAt.Time.Add(30*time.Second)) {
-		return errors.New("unauthorized")
-	}
-
-	if claims.IssuedAt == nil || claims.IssuedAt.Time.After(time.Now().Add(30*time.Second)) {
-		return errors.New("unauthorized")
-	}
-
-	return nil
 }
