3dbe293da999c65ead04ea7eed921b268808d5d0
aDiThYa-808 <adithyas824@gmail.com>
2025-12-02 20:41:55 +0530

    chore: added the access token type here

--- server/internal/jwtx/test_helpers.go
+++ server/internal/jwtx/test_helpers.go
@@ -6,6 +6,12 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
+type accessTokenClaims struct {
+	Sub   string `json:"sub"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
 func generateTestToken(t *testing.T, signingKey string, claims jwt.Claims) string {
 	testToken, tokenErr := GenerateAccessToken(signingKey, claims)
 	if tokenErr != nil {
