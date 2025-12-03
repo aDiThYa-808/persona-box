6e81351755446b1ff24869f0bdd9b54860af0571
aDiThYa-808 <adithyas824@gmail.com>
2025-12-03 13:31:30 +0530

    feat: bring google jwt claims type to jwtx

--- server/internal/jwtx/claims.go
+++ server/internal/jwtx/claims.go
@@ -2,6 +2,13 @@ package jwtx
 
 import "github.com/golang-jwt/jwt/v4"
 
+type GoogleClaims struct {
+	Sub           string `json:"sub"`
+	Email         string `json:"email"`
+	EmailVerified bool   `json:"email_verified"`
+	jwt.RegisteredClaims
+}
+
 type AccessTokenClaims struct {
 	Sub   string `json:"sub"`
 	Email string `json:"email"`
