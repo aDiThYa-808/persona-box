a410870f26525528461008fda8c2d476875971be
aDiThYa-808 <adithyas824@gmail.com>
2025-12-04 18:39:16 +0530

    feat: add user model

---
+++ server/pkg/models/user.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type User struct {
+	UserID      string    `json:"user_id"`
+	Email       string    `json:"email"`
+	DisplayName string    `json:"display_name"`
+	Picture     string    `json:"picture"`
+	CreatedAt   time.Time `json:"created_at"`
+}
