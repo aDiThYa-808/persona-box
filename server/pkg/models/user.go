7b2a58a96e8ecf32f183f5388a58de7a73380618
aDiThYa-808 <adithyas824@gmail.com>
2025-12-05 19:50:35 +0530

    feat: change CreatedAt type to string(ISO) from time.Time

--- server/pkg/models/user.go
+++ server/pkg/models/user.go
@@ -1,11 +1,9 @@
 package models
 
-import "time"
-
 type User struct {
-	UserID      string    `json:"user_id"`
-	Email       string    `json:"email"`
-	DisplayName string    `json:"display_name"`
-	Picture     string    `json:"picture"`
-	CreatedAt   time.Time `json:"created_at"`
+	UserID      string `json:"user_id"`
+	Email       string `json:"email"`
+	DisplayName string `json:"display_name"`
+	Picture     string `json:"picture"`
+	CreatedAt   string `json:"created_at"`
 }
