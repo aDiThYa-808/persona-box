72c4d1bd7ec9dc666d717b0999eb282f17e28128
aDiThYa-808 <adithyas824@gmail.com>
2025-12-15 20:35:18 +0530

    feat: write helper to call DescribeTable

---
+++ server/internal/dynamodb/describe.go
@@ -0,0 +1,21 @@
+package dynamodb
+
+import (
+	"context"
+	"errors"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func DescribeTable(ctx context.Context, name string) error {
+	_, err := DB.DescribeTable(ctx, &dynamodb.DescribeTableInput{
+		TableName: aws.String(name),
+	})
+
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	return nil
+}
