85e5cecc0ddbfe4a57b71fa91f79cf48a1e6b8d4
aDiThYa-808 <adithyas824@gmail.com>
2025-12-16 23:01:15 +0530

    feat: wrote helper to create new user item in dynamodb table

--- server/internal/dynamodb/user.go
+++ server/internal/dynamodb/user.go
@@ -1,7 +1,41 @@
 package dynamodb
 
 import (
+	"context"
+	"errors"
+
 	"github.com/aDiThYa-808/persona-box/internal/models"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
-func CreateNewUser(user models.User) {}
+func CreateNewUser(ctx context.Context, user models.User) (created bool, error error) {
+
+	putParams := &dynamodb.PutItemInput{
+		TableName: aws.String("User"),
+		Item: map[string]types.AttributeValue{
+			"UserID":      &types.AttributeValueMemberS{Value: user.UserID},
+			"Email":       &types.AttributeValueMemberS{Value: user.Email},
+			"DisplayName": &types.AttributeValueMemberS{Value: user.DisplayName},
+			"Picture":     &types.AttributeValueMemberS{Value: user.Picture},
+			"CreatedAt":   &types.AttributeValueMemberS{Value: user.CreatedAt},
+		},
+		ConditionExpression: aws.String("attribute_not_exists(UserID)"),
+	}
+
+	_, putErr := DB.PutItem(ctx, putParams)
+
+	if putErr != nil {
+		//error type for conditional expression not satisfied
+		var ccfe *types.ConditionalCheckFailedException
+
+		if errors.As(putErr, &ccfe) {
+			//creation fails but no error. expected behaviour
+			return false, nil
+		}
+		return false, putErr
+	}
+
+	return true, nil
+}
