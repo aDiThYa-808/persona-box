6b5f2225d100639bb46816ab1620951208b1a754
aDiThYa-808 <adithyas824@gmail.com>
2025-12-17 23:21:43 +0530

    feat: move models into dynamodb package

--- server/internal/dynamodb/chatsession.go
+++ server/internal/dynamodb/chatsession.go
@@ -1,7 +1,7 @@
 package dynamodb
 
 import (
-	"github.com/aDiThYa-808/persona-box/internal/models"
+	"github.com/aDiThYa-808/persona-box/internal/dynamodb/models"
 )
 
 func CreateNewChatSession(session models.ChatSession) {}
