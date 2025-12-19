4784098cf4383614a73ca237c66a468ebd987e35
aDiThYa-808 <adithyas824@gmail.com>
2025-12-19 18:27:09 +0530

    feat: add helper to get item from User table

--- server/internal/dynamodbx/user.go
+++ server/internal/dynamodbx/user.go
@@ -6,10 +6,17 @@ import (
 
 	"github.com/aDiThYa-808/persona-box/internal/dynamodbx/models"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
+/*
+Creates a new item in the User table using PutItem().
+Returns created = true if item is added successfully.
+Returns created = false if item already exists with the same user.UserID.
+Returns error is PutItem() fails.
+*/
 func CreateNewUser(ctx context.Context, user models.User) (created bool, error error) {
 
 	putParams := &dynamodb.PutItemInput{
@@ -37,4 +44,36 @@ func CreateNewUser(ctx context.Context, user models.User) (created bool, error e
 	}
 
 	return true, nil
+}
+
+/*
+Gets the item from User table that has the same partition key value as the provided userID using GetItem().
+Return user if it is found.
+Returns error if the item doesnt exist or GetItem() fails.
+*/
+func GetUser(ctx context.Context, userID string) (user models.User, error error) {
+	getParams := &dynamodb.GetItemInput{
+		TableName: aws.String("User"),
+		Key: map[string]types.AttributeValue{
+			"UserID": &types.AttributeValueMemberS{Value: userID},
+		},
+	}
+
+	resp, getErr := DB.GetItem(ctx, getParams)
+
+	if getErr != nil {
+		return models.User{}, getErr
+	}
+	if resp.Item == nil {
+		return models.User{}, errors.New("user doesnt exist")
+	}
+
+	var usr models.User
+
+	//reads the resp.Item map and fills the feilds of usr.
+	unmarshalErr := attributevalue.UnmarshalMap(resp.Item, &usr)
+	if unmarshalErr != nil {
+		return models.User{}, unmarshalErr
+	}
+	return usr, nil
 }
