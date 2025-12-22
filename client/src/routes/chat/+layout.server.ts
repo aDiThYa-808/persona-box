dcb4d5f5b21a74c306b92ab1f574ee3cbd470b2d
aDiThYa-808 <adithyas824@gmail.com>
2025-12-22 20:03:01 +0530

    feat: add type for load function

--- client/src/routes/chat/+layout.server.ts
+++ client/src/routes/chat/+layout.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from '@sveltejs/kit'
+import type {LayoutServerLoad} from "./$types"
 
-export const load = ({locals}) => {
+export const load:LayoutServerLoad = ({locals}) => {
     if(!locals.user){
        throw redirect(302,"/login") 
     }
