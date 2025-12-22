e1e80d5b66014656166f6170684942e9e813d0ce
aDiThYa-808 <adithyas824@gmail.com>
2025-12-22 20:04:24 +0530

    feat: call the api/auth/user endpoint to fetch user details on page load

--- client/src/routes/dashboard/+layout.server.ts
+++ client/src/routes/dashboard/+layout.server.ts
@@ -1,11 +1,25 @@
-import { redirect } from '@sveltejs/kit'
+import { error, redirect } from '@sveltejs/kit';
+import { PUBLIC_BASE_URL, PUBLIC_ENVIRONMENT } from '$env/static/public';
+import type { LayoutServerLoad } from './$types';
 
-export const load = ({locals}) => {
-    if(!locals.user){
-       throw redirect(302,"/login") 
-    }
+export const load: LayoutServerLoad = async ({ locals, fetch }) => {
+	if (!locals.user) {
+		throw redirect(302, '/login');
+	}
+	try {
+		const res = await fetch(`api/auth/user`);
 
-    return {
-        user: locals.user
-    }
-}
+		if (!res.ok) {
+			throw error(res.status, 'user not found');
+		}
+
+		const data = await res.json();
+
+		return {
+			name: data.display_name,
+			email: data.email
+		};
+	} catch (err) {
+		throw redirect(302, '/login');
+	}
+};
