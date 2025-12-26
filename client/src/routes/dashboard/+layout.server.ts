3eaddee95722e0de529aea00ce26a8c2e1491edf
aDiThYa-808 <adithyas824@gmail.com>
2025-12-26 18:47:38 +0530

    feat: use user type for page data

--- client/src/routes/dashboard/+layout.server.ts
+++ client/src/routes/dashboard/+layout.server.ts
@@ -1,4 +1,5 @@
 import { error, redirect } from '@sveltejs/kit';
+import type { User } from '$lib/types/user';
 import { PUBLIC_BASE_URL, PUBLIC_ENVIRONMENT } from '$env/static/public';
 import type { LayoutServerLoad } from './$types';
 
@@ -15,10 +16,12 @@ export const load: LayoutServerLoad = async ({ locals, fetch }) => {
 
 		const data = await res.json();
 
-		return {
+		const user: User = {
 			name: data.display_name,
 			email: data.email
 		};
+
+		return user;
 	} catch (err) {
 		throw redirect(302, '/login');
 	}
