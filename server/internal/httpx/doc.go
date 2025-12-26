c3ca864f48e8e88b1ba7d8ef5761e931dd0f7778
aDiThYa-808 <adithyas824@gmail.com>
2025-12-26 16:37:28 +0530

    fix: update extractor to extract token from request cookies

--- server/internal/httpx/doc.go
+++ server/internal/httpx/doc.go
@@ -1,7 +1,7 @@
 /*
 Contains helper methods to handle http requests and responses.
 Exposes the following three methods:
-ExtractToken() extracts access token from the request header and removes "bearer " prefix;
+ExtractToken() extracts access token from the request cookies and returns. error is returned if cookie doesnt exist;
 WriteJSONError() returns an http response with the provided error message and status code;
 WriteJSONSuccess() returns an http success response with the provided content s of type any;
 */
--- server/internal/httpx/extractor.go
+++ server/internal/httpx/extractor.go
@@ -3,21 +3,15 @@ package httpx
 import (
 	"errors"
 	"net/http"
-	"strings"
 )
 
-// Extracts access token from request header and removes the "Bearer " prefix.
+// Extracts access token from request cookies and return it. Returns error if not found.
 func ExtractToken(r *http.Request) (string, error) {
-	header := r.Header.Get("Authorization")
-	if header == "" {
-		return "", errors.New("authorization header missing")
+	token, err := r.Cookie("pb_access_token")
+
+	if err != nil {
+		return "", errors.New("token missing")
 	}
 
-	accessToken, exists := strings.CutPrefix(header, "Bearer ")
-
-	if !exists || accessToken == "" {
-		return "", errors.New("invalid authorization header")
-	}
-
-	return accessToken, nil
+	return token.Value, nil
 }
