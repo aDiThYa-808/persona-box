9c07da47156bcd8b2d17d9456e603d0f139bce14
aDiThYa-808 <adithyas824@gmail.com>
2025-12-29 15:28:53 +0530

    feat: add new page to create persona

---
+++ client/src/routes/chat/new/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PersonaData } from '$lib/types/persona';
+	import Create from '$lib/components/create.svelte';
+
+	function createPersona(data: PersonaData) {
+		console.log(data);
+	}
+</script>
+
+<Create {createPersona} />
