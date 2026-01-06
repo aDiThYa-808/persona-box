0bcc355616fadf399c4a62d7fafdf8a105b5d17e
aDiThYa-808 <adithyas824@gmail.com>
2026-01-06 23:38:59 +0530

    feat: write handler to create persona

--- server/internal/handlers/createpersona.go
+++ server/internal/handlers/createpersona.go
@@ -1,8 +1,11 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/aDiThYa-808/persona-box/internal/dynamodbx"
+	"github.com/aDiThYa-808/persona-box/internal/dynamodbx/models"
 	"github.com/aDiThYa-808/persona-box/internal/httpx"
 	"github.com/aDiThYa-808/persona-box/internal/jwtx"
 )
@@ -13,8 +16,32 @@ func CreatePersonaHandler(w http.ResponseWriter, r *http.Request) {
 		httpx.WriteJSONError(w, "unauthorized", http.StatusUnauthorized)
 		return
 	}
-	_, validateErr := jwtx.ValidateAccessToken(token)
+	claims, validateErr := jwtx.ValidateAccessToken(token)
 	if validateErr != nil {
 		httpx.WriteJSONError(w, "unauthorized", http.StatusUnauthorized)
+		return
+	}
+
+	var req models.Persona
+	decodeErr := json.NewDecoder(r.Body).Decode(&req)
+	if decodeErr != nil {
+		httpx.WriteJSONError(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	ctx := r.Context()
+	userID := claims.Sub
+	personaID := "" // create unique id
+
+	persona := models.Persona{
+		UserID:    userID,
+		PersonaID: personaID,
+		//add the remaining fields
+	}
+
+	createErr := dynamodbx.CreateNewPersona(ctx, persona)
+	if createErr != nil {
+		httpx.WriteJSONError(w, "failed to create persona", http.StatusInternalServerError)
+		return
 	}
 }
