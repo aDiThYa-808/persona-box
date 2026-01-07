c31bdf976bf0da001b25f20049c01c9d9fe0d2a7
aDiThYa-808 <adithyas824@gmail.com>
2026-01-07 19:03:48 +0530

    feat: call the proxy api to create persona

--- client/src/routes/chat/new/+page.svelte
+++ client/src/routes/chat/new/+page.svelte
@@ -2,8 +2,26 @@
 	import type { PersonaData } from '$lib/types/persona';
 	import Create from '$lib/components/create.svelte';
 
-	function createPersona(data: PersonaData) {
-		console.log(data);
+	async function createPersona(data: PersonaData) {
+		try {
+			const res = await fetch(`/api/create-persona`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(data)
+			});
+
+			if (!res.ok) {
+				throw new Error(`HTTP Error. Status: ${res.status}`);
+			}
+
+			const resData = await res.json();
+
+			console.log(JSON.stringify(resData));
+		} catch (err) {
+			console.log(err);
+		}
 	}
 </script>
 
