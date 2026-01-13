26fa41c9097805835877caf25916b08b40d10632
aDiThYa-808 <adithyas824@gmail.com>
2026-01-13 19:39:55 +0530

    feat: add method to create chat and pass it to child component

--- client/src/routes/chat/[personaid]/+page.svelte
+++ client/src/routes/chat/[personaid]/+page.svelte
@@ -6,7 +6,28 @@
     export let data
     $: personaName = $personas.find(p=>p.persona_id === data.personaid)?.name
     let chatList: Chat[] = []
-</script>
 
-<Chatlist personaName={personaName} chatList={chatList}/>
+    async function startNewChat(){
+        try{
+            const body = JSON.stringify({"persona_id":data.personaid})
+            const res = await fetch('/api/start-chat',{
+                method:'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: body
+            })
+
+            if(!res.ok){
+                const data = await res.json()
+                throw new Error(`HTTP error. status:${res.status}; message:${data.error}`)
+            }
+
+            // navigate to chat/[personaid]/[chatid] . use the chatid from response
+        }catch(err){
+            console.log(err)
+        }
+    }
+</script>
+<Chatlist personaName={personaName} chatList={chatList} {startNewChat}/>
 
