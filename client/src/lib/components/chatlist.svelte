7287566ae457c79880d07355ea509747dfb766db
aDiThYa-808 <adithyas824@gmail.com>
2026-01-13 19:39:14 +0530

    feat: add method prop to call when new chat btn is clicked

--- client/src/lib/components/chatlist.svelte
+++ client/src/lib/components/chatlist.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import type { Chat } from '$lib/types/chat';
 
+    export let startNewChat : () => void
 	export let personaName;
 	export let chatList: Chat[]
+
+    function handleNewChat(){
+        startNewChat()
+    }
 </script>
 
 <div class="flex h-screen flex-col bg-background text-text lg:ml-72">
@@ -17,6 +22,8 @@
 			<!-- New Chat Button -->
 			<button
 				class="mb-8 w-full rounded-2xl border border-white/10 bg-card px-4 py-3 text-center text-lg text-text transition hover:bg-card/80"
+                on:click={handleNewChat}
+                on:keydown={(e)=> e.key === "Enter" && handleNewChat()}
 			>
 				Start a new chat
 			</button>
