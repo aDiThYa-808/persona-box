96976c4c3ed16e9474bf92425310797cb4a4476e
aDiThYa-808 <adithyas824@gmail.com>
2026-01-24 19:41:03 +0530

    feat: update message and token count

--- server/internal/handlers/chat.go
+++ server/internal/handlers/chat.go
@@ -33,7 +33,7 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_, validateErr := jwtx.ValidateAccessToken(accessToken)
+	claims, validateErr := jwtx.ValidateAccessToken(accessToken)
 	if validateErr != nil {
 		httpx.WriteJSONError(w, validateErr.Error(), http.StatusUnauthorized)
 		return
@@ -53,7 +53,7 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 
 	response := ChatResponse{}
 
-	persona, getPersonaErr := dynamodbx.GetPersonaByPersonaID(ctx, req.PersonaID)
+	persona, getPersonaErr := dynamodbx.GetPersonaByPersonaID(ctx, claims.Sub, req.PersonaID)
 	if getPersonaErr != nil {
 		log.Println(getPersonaErr)
 		httpx.WriteJSONError(w, "persona doesnt exist", http.StatusNotFound)
@@ -72,7 +72,7 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if req.SessionID == "" {
-		title, summary, err := openaiadapter.GenerateTitleAndSummary(ctx, "")
+		title, summary, err := openaiadapter.GenerateTitleAndSummary(ctx, userMessage)
 		if err != nil {
 			log.Println(err)
 			title = persona.PersonaName + " - New Chat"
@@ -85,7 +85,7 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 			Title:        title,
 			CreatedAt:    time.Now().UTC().Format(time.RFC3339),
 			UpdatedAt:    time.Now().UTC().Format(time.RFC3339),
-			MessageCount: 1,
+			MessageCount: 2,
 			TokenCount:   int(tokensUsed),
 			Summary:      summary,
 		}
@@ -102,16 +102,24 @@ func ChatHandler(w http.ResponseWriter, r *http.Request) {
 	} else {
 		chatSession, getChatSessionErr := dynamodbx.GetChatSessionBySessionID(ctx, req.SessionID)
 		if getChatSessionErr != nil {
+			log.Println(getChatSessionErr)
 			httpx.WriteJSONError(w, "chat session not found", http.StatusNotFound)
 			return
 		}
 
-		if chatSession.TokenCount > 7000 {
+		if chatSession.TokenCount+int(tokensUsed) > 7000 { // add message limit after deciding the limit.
 			httpx.WriteJSONError(w, "chat limit reached", http.StatusTooManyRequests)
 			return
 		}
 
 		// update summary after every N number of messages. N tbd
+
+		updateErr := dynamodbx.UpdateSessionMessageAndTokenCount(ctx, chatSession.SessionID, time.Now().UTC().Format(time.RFC3339), 2, int(tokensUsed))
+		if updateErr != nil {
+			log.Println(updateErr)
+			httpx.WriteJSONError(w, "failed to update chat session", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	// store both the user and assistant message in the messages tables
