59984bb01f311a2bcf3d182b6b4af4c67341831f
aDiThYa-808 <adithyas824@gmail.com>
2026-01-24 19:45:49 +0530

    feat: call chat endpoint with sessionid and display response

--- client/src/routes/chat/[personaid]/[chatid]/+page.svelte
+++ client/src/routes/chat/[personaid]/[chatid]/+page.svelte
@@ -1,5 +1,41 @@
 <script lang="ts">
+	import Chat from '$lib/components/chat.svelte';
+	import type { Message } from '$lib/types/message';
+
     export let data
+
+	let chatName = 'This is the chat name';
+	let messages: Message[] = [];
+	let loading: boolean = false;
+
+	async function sendPrompt(prompt: string) {
+		messages = [...messages, { role: 'user', text: prompt }];
+
+		loading = true;
+		try {
+            let body = JSON.stringify({"session_id": data.chatid,"persona_id":data.personaid,"message":prompt})
+			const response = await fetch(`/api/chat`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: body
+			});
+
+			if (!response.ok) {
+				throw new Error(`HTTP error. Status: ${response.status}`);
+			}
+
+			const chatData = await response.json();
+        
+
+			messages = [...messages, { role: 'assistant', text: chatData.response }];
+		} catch (err) {
+			messages = [...messages, { role: 'assistant', text: (err as Error).message }];
+		} finally {
+			loading = false;
+		}
+	}
 </script>
 
-<h1>{data.chatid}</h1>
+<Chat {chatName} {messages} {sendPrompt} {loading} />
