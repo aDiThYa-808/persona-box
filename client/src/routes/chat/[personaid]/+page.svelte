8f6e9afadfa01ce77750b91c646bcf37a104a8c0
aDiThYa-808 <adithyas824@gmail.com>
2026-01-24 19:45:17 +0530

    feat: call chat api without sessionid to create one

--- client/src/routes/chat/[personaid]/+page.svelte
+++ client/src/routes/chat/[personaid]/+page.svelte
@@ -1,33 +1,41 @@
 <script lang="ts">
-	import type {Chat} from '$lib/types/chat.ts'; 
+	import type {Chat, NewChatResponse} from '$lib/types/chat.ts'; 
     import Chatlist from '$lib/components/chatlist.svelte';
 	import { personas } from '../../../stores/personas.js';
+	import { goto } from '$app/navigation';
+	import { resolve } from '$app/paths';
 
     export let data
     $: personaName = $personas.find(p=>p.persona_id === data.personaid)?.name
     let chatList: Chat[] = []
 
-    async function startNewChat(){
-        console.log("Work in progress, lol")
-        // try{
-        //     const body = JSON.stringify({"persona_id":data.personaid})
-        //     const res = await fetch('/api/start-chat',{
-        //         method:'POST',
-        //         headers: {
-        //             'Content-Type':'application/json'
-        //         },
-        //         body: body
-        //     })
+    async function startNewChat(message: string){
+        try{
+            const body = JSON.stringify({"persona_id":data.personaid, "message":message})
+            const res = await fetch('/api/chat',{
+                method:'POST',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: body
+            })
 
-        //     if(!res.ok){
-        //         const data = await res.json()
-        //         throw new Error(`HTTP error. status:${res.status}; message:${data.error}`)
-        //     }
+            if(!res.ok){
+                const errorData = await res.json()
+                throw new Error(`HTTP error. status:${res.status}; message:${errorData.error}`)
+            }
 
-        //     // navigate to chat/[personaid]/[chatid] . use the chatid from response
-        // }catch(err){
-        //     console.log(err)
-        // }
+            const chatResponse : NewChatResponse = await res.json()
+            console.log(chatResponse)
+
+            goto(resolve(`/chat/${data.personaid}/${chatResponse.session_id}`),{
+				replaceState:true,
+				noScroll:false
+			})
+
+        }catch(err){
+            console.log(err)
+        }
     }
 </script>
 <Chatlist personaName={personaName} chatList={chatList} {startNewChat}/>
