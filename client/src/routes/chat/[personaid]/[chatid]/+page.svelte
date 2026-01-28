5a19d253f6889e4fac3d2ddb939c676b3a2b2bb9
aDiThYa-808 <adithyas824@gmail.com>
2026-01-28 21:14:18 +0530

    feat: check is its a new chat before calling backend

--- client/src/routes/chat/[personaid]/[chatid]/+page.svelte
+++ client/src/routes/chat/[personaid]/[chatid]/+page.svelte
@@ -1,19 +1,39 @@
 <script lang="ts">
+	import { replaceState } from '$app/navigation';
+	import { page } from '$app/stores';
 	import Chat from '$lib/components/chat.svelte';
+	import type { NewChatResponse } from '$lib/types/chat.js';
 	import type { Message } from '$lib/types/message';
+	import { onMount } from 'svelte';
+	import { personas } from '../../../../stores/personas.js';
 
-    export let data
+	export let data;
+	$: personaName = $personas.find((p) => p.persona_id === data.personaid)?.name;
+	$: chatName = 'New Chat';
 
-	let chatName = 'This is the chat name';
+	$: title = personaName + " - "+ chatName
+
+	$: chatID = data.chatid
 	let messages: Message[] = [];
 	let loading: boolean = false;
+
+	onMount(async()=>{
+		const firstMessage = $page.state?.message
+		if(firstMessage){
+			sendPrompt(firstMessage)
+			console.log(firstMessage)
+		} // else do something to show failure
+	})
 
 	async function sendPrompt(prompt: string) {
 		messages = [...messages, { role: 'user', text: prompt }];
 
 		loading = true;
 		try {
-            let body = JSON.stringify({"session_id": data.chatid,"persona_id":data.personaid,"message":prompt})
+			let body = (chatID != "new-chat" && chatID != "")
+				? JSON.stringify({ session_id: chatID, persona_id: data.personaid, message: prompt })
+				: JSON.stringify({ persona_id: data.personaid, message: prompt });
+
 			const response = await fetch(`/api/chat`, {
 				method: 'POST',
 				headers: {
@@ -26,16 +46,22 @@
 				throw new Error(`HTTP error. Status: ${response.status}`);
 			}
 
-			const chatData = await response.json();
-        
+			const chatData: NewChatResponse = await response.json();
+
+			if(chatData.session_id){
+				replaceState(`/chat/${data.personaid}/${chatData.session_id}`, $page.state)
+			}
+			if(chatData.title){
+				chatName = chatData.title
+			}
 
 			messages = [...messages, { role: 'assistant', text: chatData.response }];
 		} catch (err) {
-			messages = [...messages, { role: 'assistant', text: (err as Error).message }];
+			messages = [...messages, { role: 'assistant', text: "Something went wrong on our end. Please try sending your message again." }];
 		} finally {
 			loading = false;
 		}
 	}
 </script>
 
-<Chat {chatName} {messages} {sendPrompt} {loading} />
+<Chat chatName={title} {messages} {sendPrompt} {loading} />
