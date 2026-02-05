9abc153ac0e36f0caadd4280667f2264454e43e1
aDiThYa-808 <adithyas824@gmail.com>
2026-02-05 22:35:13 +0530

    fix: remove unused type

--- server/internal/dynamodbx/models/message.go
+++ server/internal/dynamodbx/models/message.go
@@ -3,6 +3,6 @@ package models
 type ChatMessage struct {
 	SessionID string `json:"session_id"` //PK
 	CreatedAt string `json:"created_at"` //SK ISO string
-	Sender    string `json:"sender"`
-	Content   string `json:"content"`
+	Role      string `json:"role"`
+	Message   string `json:"message"`
 }
