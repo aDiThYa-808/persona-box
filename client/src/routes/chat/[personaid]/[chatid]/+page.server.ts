42877c8774fdb867d1144a2b09c65c3ae8966acc
aDiThYa-808 <adithyas824@gmail.com>
2026-02-05 22:43:31 +0530

    feat: fetch chat messages on load

---
+++ client/src/routes/chat/[personaid]/[chatid]/+page.server.ts
@@ -0,0 +1,37 @@
+import { PUBLIC_AWS_INVOKE_URL } from '$env/static/public';
+import type { PageServerLoad } from './$types';
+import type { Message } from '$lib/types/message';
+
+export const load: PageServerLoad = async ({ params, fetch }) => {
+	try {
+		const invokeUrl = PUBLIC_AWS_INVOKE_URL;
+		const chatId = params.chatid;
+
+		if (chatId == 'new-chat') {
+			return {
+				messages: []
+			};
+		}
+
+		const res = await fetch(`/api/messages?sessionid=${chatId}`);
+
+		if (!res.ok) {
+			let data = res.json();
+			console.log(`Failed to fetch messages. ${JSON.stringify(data)}`);
+			return {
+				messages: []
+			};
+		}
+
+		const sessionMessages: Message[] = await res.json();
+
+		return {
+			messages: sessionMessages.sort((a, b) => b.created_at.localeCompare(a.created_at))
+		};
+	} catch (err) {
+		console.log(err);
+		return {
+			messages: []
+		};
+	}
+};
