f02eb43c94cd64dce387351f43914b6d5af450cc
aDiThYa-808 <adithyas824@gmail.com>
2026-02-08 18:51:12 +0530

    fix:rename file

--- client/src/stores/store.ts
+++ client/src/stores/store.ts
@@ -1,8 +1,10 @@
 import type { Chat } from "$lib/types/chat";
 import type { Message } from "$lib/types/message";
 import type { PersonaList } from "$lib/types/persona";
+import type { User } from "$lib/types/user";
 import { writable } from "svelte/store";
 
+export const user = writable<User>()
 export const personas = writable<PersonaList[]>([])
 export const chats = writable<Chat[]>([])
 export const messages = writable<Message[]>([])
