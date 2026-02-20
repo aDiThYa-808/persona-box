f801aedbceeb2bc062cafa96dc9e077288e19445
aDiThYa-808 <adithyas824@gmail.com>
2026-02-20 23:17:00 +0530

    feat: add endpoint to logout

---
+++ client/src/routes/api/auth/logout/+server.ts
@@ -0,0 +1,6 @@
+import { redirect, type RequestEvent } from "@sveltejs/kit";
+
+export async function GET(event : RequestEvent){
+    event.cookies.delete('pb_access_token',{path:"/"})
+    return new Response(JSON.stringify({message:"deleted cookie"}),{status: 200})
+}
